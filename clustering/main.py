e4fc7f067ebe4e198c80259a3e7f126dc3fb5330
harrylyx <lyxvae@gmail.com>
2016-08-14 11:58:57 +0800

    change coding

--- clustering/main.py
+++ clustering/main.py
@@ -23,10 +23,15 @@ def get_avg(data):
 
 
 def get_mode(data):
+    '''
+    计算众数
+    :param data:一维list
+    :return: list中的众数
+    '''
     mode = []   # 用于储存众数
     age_appear = dict((a, data.count(a)) for a in data)  # 统计各个元素出现的次数
     if max(age_appear.values()) == 1:  # 如果最大的出现为1
-        for k, v in age_appear.items():
+        for k, v in age_appear.items():     # 就返回随机一个数
             mode.append(k)
             return mode
     else:
@@ -35,17 +40,43 @@ def get_mode(data):
                 mode.append(k)
     return mode
 
-def ed_relate(dataX, dataY):
+
+def ed_relate_f(dataX, dataY):      # 计算浮点型欧氏距离
     '''
     :param dataX:第一行
     :param dataY: 第二行
     :return: 之间的相似度
     '''
-    sum = 0
+    summ = 0
     if len(dataX) == len(dataY):
         for a in range(0, len(dataX)):
-            sum += (float(dataX[a])-float(dataY[a])) ** 2
-        relate = math.sqrt(sum)
+            if a == 0:
+                if dataX[a] != dataY[a]:
+                    summ = 2
+                continue
+            summ += (float(dataX[a])-float(dataY[a])) ** 2
+        relate = math.sqrt(summ)
+        return relate
+    else:
+        print 'len is not equal'
+        return 0
+
+
+def ed_relate(dataX, dataY):    # 计算整型欧氏距离
+    '''
+    :param dataX:第一行
+    :param dataY: 第二行
+    :return: 之间的相似度
+    '''
+    summ = 0
+    if len(dataX) == len(dataY):
+        for a in range(0, len(dataX)):
+            if a == 0:
+                if dataX[a] != dataY[a]:
+                    summ = 2
+                continue
+            summ += (dataX[a]-dataY[a]) ** 2
+        relate = math.sqrt(summ)
         return relate
     else:
         print 'len is not equal'
@@ -80,7 +111,7 @@ def km_first(data,k):
             if m == d:
                 continue
             else:
-                temp.append(ed_relate(m[2:], d[2:]))
+                temp.append(ed_relate_f(m[1:], d[1:]))
         ed_lenth.append(temp)
     ed_lenth = map(list, zip(*ed_lenth))
     new_data=[]
@@ -120,7 +151,7 @@ def km(data):
                 if m == d:
                     continue
                 else:
-                    temp.append(ed_relate(m[1:], d[2:]))
+                    temp.append(ed_relate_f(m[0:], d[1:]))
         ed_lenth.append(temp)       # 每个mid对应每个元素的距离
     ed_lenth = map(list, zip(*ed_lenth))    # 每个元素对应每个mid的距离
     new_data = []
@@ -135,18 +166,20 @@ def km(data):
         random_sum = 0      # 任意两点距离和
         for d1 in data:     # 计算类内距离和
             print '类内距离和计算中...'
+            d1 = [[int(x) for x in row] for row in d1]
             for i in range(len(d1)):
                 for j in range(i, len(d1)):
-                    lenth_sum += ed_relate(d1[i][2:], d1[j][2:])
+                    lenth_sum += ed_relate(d1[i][1:], d1[j][1:])
         print '类内距离和计算完毕'
         data_n = sum(data, [])
+        data_n = [[int(x) for x in row] for row in data_n]
         start1 = time.time()
         for i in range(len(data_n)):    # 计算任意两点距离和
             for j in range(i+1, len(data_n)):
-                random_sum += ed_relate(data_n[i][2:], data_n[j][2:])
+                random_sum += ed_relate(data_n[i][1:], data_n[j][1:])
         print '任意两点距离和计算完毕'
         end1 = time.time()
-        print end1 - start1
+        print '任意两点距离和计算时间:'+str(end1 - start1)
         print lenth_sum
         print random_sum
         print lenth_sum/random_sum
@@ -159,4 +192,4 @@ if __name__ == '__main__':
     data = onehot.use_one_hot(data, 1)  # 将第二列标称型换为one-hot编码
     km(km_first(data, 15))    # 进行k-means算法
     end = time.time()
-    print end - start
+    print '总计算时间:'+str(end - start)
